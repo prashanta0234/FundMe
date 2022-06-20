213cf6385f80783a1a4eb2547c9821ad275ddd4f
"Prashanta Chakraborty" <"prashanta0234@gmail.com">
2022-06-21 01:27:19 +0600

    add funders

--- Fundme.sol
+++ Fundme.sol
@@ -3,9 +3,13 @@ pragma solidity >=0.7.0<0.9.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
+    address[] funders;
+    mapping(address=>uint) public addressAmount; 
     uint public minimumValue=2*1e18;
     function Fund() public payable{
         require(getConverstion(msg.value) >minimumValue,"Please send more big value");
+        funders.push(msg.sender);
+        addressAmount[msg.sender]=msg.value;
     }
 
     function getPrice() public view returns(uint) {
